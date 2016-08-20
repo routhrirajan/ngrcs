43b17aecc5e69229b475ee99f9e7e29dad17a711
routhrirajan <routhrimanjore@yahoo.com>
2016-08-20 23:38:44 +0530

    Changes upto services

---
+++ app/mock-heroes.ts
@@ -0,0 +1,7 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: "IRCTC", weburl: "https://irctc.co.in", imageurl: "" },
+    { id: 2, name: "VIA.COM", weburl: "https://in.via.com/", imageurl: "" }
+
+] 
